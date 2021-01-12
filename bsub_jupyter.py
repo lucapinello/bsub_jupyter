a69784dacef05371859ed7b181f2880ca3c681d4
lucapinello <lucapinello@gmail.com>
2021-01-12 18:42:39 -0500

    fixed bytecode string

--- bsub_jupyter.py
+++ bsub_jupyter.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
-#!/usr/bin/env python 
+#!/usr/bin/env python
 '''
 Jupyter_Bsub - Luca Pinello & Kendell Clement 2017
 Connect to a LSF main node directly or trough a ssh jump node, launch a jupyter notebook via bsub and open automatically a tunnel.
 '''
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 
 import subprocess as sb
@@ -50,23 +50,23 @@ def query_yes_no(question, default="yes"):
                              "(or 'y' or 'n').\n")
 
 print('''
- _               _           _                   _            
-| |__  ___ _   _| |__       (_)_   _ _ __  _   _| |_ ___ _ __ 
+ _               _           _                   _
+| |__  ___ _   _| |__       (_)_   _ _ __  _   _| |_ ___ _ __
 | '_ \/ __| | | | '_ \      | | | | | '_ \| | | | __/ _ \ '__|
-| |_) \__ \ |_| | |_) |     | | |_| | |_) | |_| | ||  __/ |   
-|_.__/|___/\__,_|_.__/____ _/ |\__,_| .__/ \__, |\__\___|_|   
-                    |_____|__/      |_|    |___/             
+| |_) \__ \ |_| | |_) |     | | |_| | |_) | |_| | ||  __/ |
+|_.__/|___/\__,_|_.__/____ _/ |\__,_| .__/ \__, |\__\___|_|
+                    |_____|__/      |_|    |___/
 
 ''')
 print('\n\n[Luca Pinello 2017, send bugs, suggestions or *green coffee* to lucapinello AT gmail DOT com]\n\n')
 print( 'Version %s\n' % __version__)
- 
+
 parser = argparse.ArgumentParser(description='bsub_jupyter\n\n- Connect to a LSF main node directly or trough a ssh jump node, launch a jupyter notebook via bsub and open automatically a tunnel.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('lsf_server', type=str,  help='username@server, the server is the main LSF node used to submit jobs with bsub') 
+parser.add_argument('lsf_server', type=str,  help='username@server, the server is the main LSF node used to submit jobs with bsub')
 parser.add_argument('connection_name', type=str,  help='Name of the connection')
-                               
-    
-#OPTIONALS    
+
+
+#OPTIONALS
 parser.add_argument('--remote_path', type=str,  help='remote path to use',default='~')
 parser.add_argument('--bastion_server',  help='SSH jump server, format username@server', default=None)
 parser.add_argument('--memory', type=int,  help='Memory to request', default=64000)
@@ -76,7 +76,7 @@ parser.add_argument('--force_new_connection',  help='Ignore any existing connect
 parser.add_argument('--ignoreHostChecking',  help='Ignore known host checking. If your client-side tunnel is not created and you get a message starting "The authenticity of host {xxx} can\'t be established." try enabling this flag.', action='store_true')
 parser.add_argument('--debug',  help='Print helpful debug messages', action='store_true')
 parser.add_argument('--env', type=str, help='load a different env for python')
-    
+
 args = parser.parse_args()
 
 username,hostname_server=args.lsf_server.split('@')
@@ -94,12 +94,12 @@ if bastion_server:
         sys.exit(1)
 
     #ssh  -L 9000:eris1n2.research.partners.org:22 lp698@ssh.research.partners.org
-    
+
     #create tunnel via bastion server
     cmd_bastion_tunnel='ssh -N -f -L {0}:{1}:{2} {3} '.format(local_bastion_port,hostname_server,ssh_port,bastion_server)
     if args.debug : print(cmd_bastion_tunnel)
     sb.call(cmd_bastion_tunnel,shell=True)
-    
+
     ssh_server=" {0}@{1} -p {2} ".format(username,"localhost", local_bastion_port)
 
 base_ssh_cmd="ssh "
@@ -108,7 +108,7 @@ base_ssh_cmd="ssh "
 if not hostname_resolves(hostname_server):
     print('Cannot resolve %s. Check server name and try again.' % hostname_server)
     sys.exit(1)
-    
+
 
 connection_name=args.connection_name
 connection_filename='jupyter_connection_%s' % connection_name
@@ -125,12 +125,12 @@ print('Checking if a connection alrady exists...')
 #check if the connection  exists already
 try:
     connection_status=sb.check_output('%s -t %s "[ -f %s ] && echo True|| echo False" 2> /dev/null' %(base_ssh_cmd,ssh_server, connection_filename),shell=True).decode('utf-8').strip()
-except TypeError: 
+except TypeError:
     print('%s -t %s "[ -f %s ] && echo True|| echo False" 2> /dev/null' %(base_ssh_cmd,ssh_server, connection_filename))
     exit(1)
 
 if connection_status=='True' and not args.force_new_connection:
-    
+
     print('A running job already exists!')
 
 else:
@@ -140,7 +140,7 @@ else:
 		env_cmd=' source activate {0} && '.format(args.env)
 	else:
 		env_cmd=' '
-	 
+
 	cmd_jupyter='%s -t %s "bsub  -q %s -n %d -M %d -cwd %s -R ' % (base_ssh_cmd,ssh_server,queue,n_cores,memory,remote_path) +"'rusage[mem=%d]'" % memory + " '"+env_cmd+" jupyter notebook --port=%d --no-browser '"%(random_remote_port)+' 2>&1 >%s "'%connection_filename+' 2>/dev/null'
 	if args.debug: print(cmd_jupyter)
 	sb.call( cmd_jupyter,shell=True)
@@ -148,8 +148,8 @@ else:
 	if args.debug: print(cmd_file_write)
 	sb.call(cmd_file_write,shell=True)
 	connection_status=True
-    
-try: 
+
+try:
     job_id=sb.check_output('%s %s " head -n 1 ~/%s" 2> /dev/null' % (base_ssh_cmd,ssh_server,connection_filename),shell=True).decode('utf-8').split('<')[1].split('>')[0]
 except TypeError:
     print('%s %s " head -n 1 ~/%s" 2> /dev/null' % (base_ssh_cmd,ssh_server,connection_filename))
@@ -172,11 +172,11 @@ while server is None:
     if args.debug: print("bjob_command: " + bjob_command)
     p = sb.Popen(bjob_command, stdout=sb.PIPE, stderr=sb.PIPE,shell=True)
     out, err = p.communicate()
-
+    #print (out, type(out))
     print('.',end = "")
     sys.stdout.flush()
 
-    m = re.search('<(.*)>, Execution Home', out)
+    m = re.search('<(.*)>, Execution Home', out.decode('utf-8'))
 
     try:
         server =  m.groups()[0].split('*')[-1]
@@ -187,18 +187,18 @@ print('\nServer launched on node: '+server)
 
 print('Local port: %d  remote port: %d' %(random_local_port, random_remote_port))
 
-if sb.check_output("nc -z localhost %d || echo 'no tunnel open';" % random_local_port,shell=True).strip()=='no tunnel open':
-    
+if sb.check_output("nc -z localhost %d || echo 'no tunnel open';" % random_local_port,shell=True).decode('utf-8').strip()=='no tunnel open':
+
 
     if query_yes_no('Should I open an ssh tunnel for you?'):
 
         sb.call('sleep 5 && python -m webbrowser -t "http://localhost:%d" & 2> /dev/null' % random_local_port,shell=True)
-        
+
         tunnel_ssh_command = "ssh "
         if args.ignoreHostChecking: tunnel_ssh_command = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
 
         cmd_tunnel = tunnel_ssh_command + " -N  -L localhost:{0}:localhost:{1} -o 'ProxyCommand ssh {2} nc %h %p'  {3}@{4}.research.partners.org 2> /dev/null".format(random_local_port,random_remote_port,ssh_server,username,server)
-    
+
         if args.debug: print(cmd_tunnel)
 
         try:
